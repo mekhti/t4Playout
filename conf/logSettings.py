05de17d71079d8c99faa859e46c028141c21aa4a
Mehdi Mammadov <mekhi@gmail.com>
2018-07-21 19:57:50 +0300

    Some improvements of conf/logSettings.py - definition of 'BASEPATH' variable changed - definition of 'formatters' fixed - definition of 'file' and 'console' handlers applied - definition of 'django' logger changed - definition of 'manage' logger applied

--- conf/logSettings.py
+++ conf/logSettings.py
@@ -1,32 +1,48 @@
 import os
+import logging
 
-BASEPATH = os.path(os.path.abspath("manage.py"))
+BASEPATH =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 LOGGING = {
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[Level: %(levelname)s]\n '
+                      '\t\t[Logger: %(name)s]\n'
+                      '\t\t[Time: %(asctime)s]\n '
+                      '\t\t[Module: %(module)s]\n '
+                      '\t\t[PID: %(process)d]\n '
+                      '\t\t[Thread: %(thread)d]\n '
+                      '\t\tMESSAGE:\n \t\t\t%(message)s'
         },
-        'logfile': {
-            'level': 'INFO',
+        'simple': {
+            'format': '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
             'class': 'logging.FileHandler',
+            'filename': os.path.join(BASEPATH, 'logs', 'manage_commands.log'),
             'formatter': 'verbose',
-            'filename': os.path.join(
-                BASEPATH, 'logs', 'manage_commands.log'),
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
         },
     },
     'loggers': {
-        '': {
-            'handlers': ['console', 'logfile'],
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
             'propagate': True,
-            'level': 'INFO',
         },
-        'django.request': {
-            'handlers': ['console', 'logfile'],
-            'propagate': False,
-            'level': 'ERROR',  # WARN also shows 404 errors
-        },
-    }
+        'manage': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
 }
